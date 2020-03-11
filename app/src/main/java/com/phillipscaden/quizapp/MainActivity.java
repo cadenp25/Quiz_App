d7e805d9b1daaae4b78aaa94319ad8e447b369fb
cadenphillips <cadephillips22@gmail.com>
2020-03-11 13:59:32 -0400

    Lab_06b

--- app/src/main/java/com/phillipscaden/quizapp/MainActivity.java
+++ app/src/main/java/com/phillipscaden/quizapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.phillipscaden.quizapp;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,16 +14,85 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-     TextView displayText;
+    TextView displayText;
 
     int it;
+    String red = "red", green = "green", blue = "blue";
+    EditText editText = findViewById(R.id.editText);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         SeekBar seekRed, seekBlue, seekBlue;
+        SeekBar seekRed, seekGreen, seekBlue;
+        seekRed = findViewById(R.id.seekred);
+        seekBlue = findViewById(R.id.seekblue);
+        seekGreen = findViewById(R.id.seekgreen);
         setContentView(R.layout.activity_main);
-        displayText=findViewById(R.id.textBox);
+        displayText = findViewById(R.id.textBox);
+        SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
 
+        editor.apply();
+        if(editText.hasFocus())
+        {
+            editText.setTextColor(Color.RED);
+        }
 
+        seekRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            editor.putInt(red, progress);
+                editor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+
+        seekGreen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                editor.putInt(green, progress);
+                editor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+
+        seekBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                editor.putInt(blue, progress);
+                editor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
     }
 }
