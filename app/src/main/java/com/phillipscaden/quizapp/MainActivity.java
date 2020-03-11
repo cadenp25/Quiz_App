863a091f629b91ea158240ebfc066b8b8d56d4a8
cadenphillips <cadephillips22@gmail.com>
2020-03-11 13:19:10 -0400

    Lab_06a

--- app/src/main/java/com/phillipscaden/quizapp/MainActivity.java
+++ app/src/main/java/com/phillipscaden/quizapp/MainActivity.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.view.View;
-import android.util.Log;
-import android.widget.EditText;
+import android.content.SharedPreferences;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.graphics.Color;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,44 +20,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         Button submitButton1, submitButton2, submitButton3;
+         SeekBar seekRed, seekBlue, seekBlue;
         setContentView(R.layout.activity_main);
         displayText=findViewById(R.id.textBox);
-        submitButton1=findViewById(R.id.clickButtonR);
-        submitButton2=findViewById(R.id.clickButtonG);
-        submitButton3=findViewById(R.id.clickButtonB);
-        submitButton1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                it++;
-                System.out.println("roja");
-                 Log.i("r", "red" + it);
-                displayText.setTextColor(Color.RED);
-                displayText.setText(" " + it);
-            }
-        });
 
-        submitButton2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                it++;
-                System.out.println("verde");
-                Log.i("g", "green" + it);
-                displayText.setTextColor(Color.GREEN);
-                displayText.setText(" " + it);
-            }
-        });
-
-        submitButton3.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                it++;
-                System.out.println("azul");
-                Log.i("b", "blue" + it);
-                displayText.setTextColor(Color.BLUE);
-                displayText.setText(" " + it);
-            }
-        });
 
     }
 }
